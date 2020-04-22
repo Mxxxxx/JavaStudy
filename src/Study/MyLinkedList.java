d01b7020cd1e68e7c464ecce8adee37ab1109048
孟鑫 <907008127@qq.com>
2020-04-22 15:59:15 +0800

    提交

---
+++ src/Study/MyLinkedList.java
@@ -0,0 +1,82 @@
+package Study;
+
+/**
+ * program: Java
+ * description:
+ * author: Mx
+ * create: 2020-04-20 18:56
+ **/
+//节点
+class Node {
+    public int data;//0
+    public Node next;//null
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class MyLinkedList {
+    public Node head;//保存单链表的头节点的引用
+
+    //头插法
+    public void addFirst(int data) {
+        Node node = new Node(data);
+        if (this.head == null) {  //第一次插入
+            this.head = node;
+            return;
+        }
+        node.next = this.head;  //要先绑后面
+        this.head = node;
+    }
+
+    //尾插法
+    public void addLast(int data) {
+        Node node = new Node(data);
+        if (this.head == null) {
+            this.head = node;
+            return;
+        }
+        Node cur = this.head;
+        while (cur.next != null) {
+            cur = cur.next;
+        }
+        cur.next = node;
+    }
+
+    //打印单链表
+    public void display() {
+        Node cur = this.head;
+        while (cur != null) {
+            System.out.print(cur.data + " ");
+            cur = cur.next;
+        }
+    }
+
+    //查找是否包含关键字key
+    public boolean contains(int data) {
+        Node cur = this.head;
+        while (cur != null) {
+            if (cur.data == data) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    //得到单链表长度
+    public int size() {
+        Node cur = this.head;
+        if (cur == null) {
+            return 0;
+        }
+        int count = 0;
+        while (cur != null) {
+            count++;
+            cur = cur.next;
+        }
+        return count;
+    }
+}
---
+++ src/Study/Study0420.java
@@ -0,0 +1,39 @@
+package Study;
+
+/**
+ * Program: Java
+ * description:单链表
+ * 单链表由节点组成
+ * 1.不带头节点的单向非循环链表/循环链表
+ * 2.带头节点的单向非循环链表/循环链表
+ *
+ * 每个节点都是对象
+ * int data
+ * Node next
+ * author: Mx
+ * create: 2020-04-20 18:30
+ **/
+public class Study0420 {
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList1=new MyLinkedList();
+        System.out.println("头插法");
+        myLinkedList1.addFirst(10);
+        myLinkedList1.addFirst(11);
+        myLinkedList1.addFirst(12);
+        myLinkedList1.addFirst(13);
+        myLinkedList1.display();
+        System.out.println();
+        System.out.println("尾插法");
+        MyLinkedList myLinkedList2=new MyLinkedList();
+        myLinkedList2.addLast(10);
+        myLinkedList2.addLast(11);
+        myLinkedList2.addLast(12);
+        myLinkedList2.addLast(13);
+        myLinkedList2.display();
+        System.out.println();
+        System.out.println("计算单链表长度");
+        System.out.println(myLinkedList2.size());
+        System.out.println("查找关键字，是否在单链表");
+        System.out.println(myLinkedList2.contains(11));
+    }
+}
