0f71d6a77c1009220f3faa27db069020f787656e
孟鑫 <907008127@qq.com>
2020-06-10 15:39:56 +0800

    tj

--- Practice/practice0610.java
+++ Practice/practice0610.java
@@ -18,6 +18,7 @@ public class practice0610 {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+
 //        while (in.hasNextInt()) {
         int num = in.nextInt();
         int n = func(num);
