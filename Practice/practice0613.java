880e0e0e175e8e76934de1790a661279a2df3866
孟鑫 <907008127@qq.com>
2020-06-13 09:39:09 +0800

    tj

---
+++ Practice/practice0613.java
@@ -0,0 +1,36 @@
+package JavaStudy.Practice;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * @program: Java
+ * @description:
+ * @author: Mx
+ * @create: 2020-06-13 08:44
+ **/
+public class practice0613 {
+    public static void func(String num) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < num.length(); i++) {
+            int n = num.charAt(i) - 48;
+            if (!hashMap.containsKey(n)) {
+                hashMap.put(n, 1);
+            } else {
+                hashMap.put(n, hashMap.get(n) + 1);
+            }
+        }
+        for (
+                HashMap.Entry<Integer, Integer> entry : hashMap.entrySet()) {
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String num = in.nextLine();
+        func(num);
+    }
+}
+
