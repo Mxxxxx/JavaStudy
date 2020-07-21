da21b56d7f6d5edae2c16a438ddee00a240a8c16
孟鑫 <907008127@qq.com>
2020-07-22 07:39:50 +0800

    tj

---
+++ Practice/practice0722.java
@@ -0,0 +1,30 @@
+package JavaStudy.Practice;
+
+import java.util.Scanner;
+
+/**
+ * @Author Meng Xin
+ * @Date 2020/7/22 7:28
+ */
+public class practice0722 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            boolean contain = true;
+            StringBuffer input = new StringBuffer(in.next());
+            char[] find = in.next().toCharArray();
+            for (char c : find) {
+                int index = input.indexOf(String.valueOf(c));
+                if (index != -1)
+                    input.deleteCharAt(index);
+                else {
+                    System.out.println("No");
+                    contain = false;
+                    break;
+                }
+            }
+            if (contain)
+                System.out.println("Yes");
+        }
+    }
+}
